a0c79e5c721877732111f15c4cb1c7732137a5bd
Nestor Gerardo Gonzalez Roque <nestor.ge222@gmail.com>
2021-10-25 21:53:36 -0600

    data dummy tarjetas

--- FETarjetaCredito/src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ FETarjetaCredito/src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tarjeta-credito.component.scss']
 })
 export class TarjetaCreditoComponent implements OnInit {
+  listTarjetas: any[]=[
+    {titular:'Juan Perez', numeroTarjeta: '123123123123123', fechaExpiracion: '11/12', cvv: 123},
+    {titular:'Jorge doe', numeroTarjeta: '893854739899889320477', fechaExpiracion: '11/12', cvv: 123},
+    {titular:'paco pelusa', numeroTarjeta: '8472384762647858927', fechaExpiracion: '11/12', cvv: 123},
+    {titular:'Pepe Pollo', numeroTarjeta: '90492377738947626376', fechaExpiracion: '11/12', cvv: 123}
+
+  ];
 
   constructor() { }
 
